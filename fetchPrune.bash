a19b6ce7542df376d1625738f88fe50353c68c95
BriceJamin <brice.jamin@hotmail.fr>
2013-03-08 00:54:30 +0100

    Simule la sortie de la commande git fetch --prune

---
+++ fetchPrune.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+{
+	echo "remote: Counting objects: 11, done."
+	echo "remote: Compressing objects: 100% (3/3), done."
+	echo "remote: Total 9 (delta 0), reused 0 (delta 0)"
+	echo "Unpacking objects: 100% (9/9), done."
+	echo "From /home/smallfitz/Documents/GitTests/GitAuto/Clef"
+	echo " * [new branch]      branche3   -> origin/branche3"
+	echo "   1daeeab..9788254  master     -> origin/master"
+	echo " * [new branch]      newbranche2 -> origin/newbranche2"
+	echo " x [deleted]         (none)     -> origin/branche1"
+	echo " x [deleted]         (none)     -> origin/branche2"
+} 1>&2
